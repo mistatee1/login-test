cd68cab85252f8d7729269c856f5ec64be811be0
anthony_akinmoyewa <anthony.akinmoyewa@callsign.com>
2022-08-09 22:04:03 +0100

    created login ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="Login-container">
+          <p className="Login-title">Welcome Back</p>
+          <p className="Login-subtitle">Enter your credentials to access your account.</p>
+          <div className="Login-form-container">
+            <input type="text" id="name" name="name" className="Login-email-input" required placeholder="Enter your email"></input>
+            <input type="text" id="name" name="name" className="Login-email-input" required placeholder="Enter your password"></input>
+            <button className="Login-email-button">Sign In</button>
+          </div>
+        </div>
+        <p className="Login-reset-text">Forgot your password? <a href="#" id="Login-reset-link">Reset Password</a></p>
       </header>
     </div>
   );
